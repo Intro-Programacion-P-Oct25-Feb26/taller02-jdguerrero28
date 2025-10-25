a557b85e5c5292533661878f7bc6a84b44d62320
jdguerrero28 <jdiegogueal@gmail.com>
2025-10-24 22:06:32 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author jdieg
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos basico de Java Versionado a traves de Git");
+        System.out.println("Manejado desde GitHub");
+        System.out.println("Ademas de usar GitHubClassroom");
+        
+        // TODO code application logic here
+    }
+    
+}
