fe70ba7902da085191e9e627f205e0493543695f
jdguerrero28 <jdiegogueal@gmail.com>
2025-10-24 21:56:16 -0500

    segundo commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -16,7 +16,7 @@ public class Proyecto01 {
     public static void main(String[] args) {
         
     System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
-     
+    System.out.println( "Creado por Juan Diego Guerrero Alvarado ");
         
         // TODO code application logic here
     }
